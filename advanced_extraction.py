2bd34e2b62c2b0d2a14048e6b902bcb9347867fc
Abhin B <85185063+bcijo@users.noreply.github.com>
2024-07-24 02:35:18 +0530

    changed read format

--- advanced_extraction.py
+++ advanced_extraction.py
@@ -16,8 +16,8 @@ def encode_image(image_path):
     Returns:
     str: The base64 encoded string of the image.
     """
-    # with open(image_path, "rb") as image_file:
-    return base64.b64encode(image_path.read()).decode('utf-8')
+    with open(image_path, "rb") as image_file:
+        return base64.b64encode(image_file.read()).decode('utf-8')
 
 def construct_prompt(image_path):
     """
